0203001832e1d31fd3485879bbce6efc63387887
fuyong7335 <yukie353yukie@gmail.com>
2025-12-17 21:01:09 +0900

    Add files via upload

--- services/sheets.py
+++ services/sheets.py
@@ -1,19 +1,22 @@
-def save_result(level, score):
+from datetime import datetime
+
+def save_result(email, level, score):
     try:
         client = get_client()
-        sheet = client.open_by_key("1f9nZ2SW43Q86UEH1hiAeAn10ZNf-jYWfiQbl65SB2C0").sheet1
-        sheet.append_row([level, score])
+        sheet = client.open_by_key(
+            "1f9nZ2SW43Q86UEH1hiAeAn10ZNf-jYWfiQbl65SB2C0"
+        ).sheet1
+
+        timestamp = datetime.now().isoformat()
+
+        sheet.append_row([
+            timestamp,
+            email,
+            level,
+            score
+        ])
+
     except Exception as e:
         print("=== SHEETS ERROR ===")
         print(e)
         raise
-
-from datetime import datetime
-
-timestamp = datetime.now().isoformat()
-sheet.append_row([
-    timestamp,
-    email,
-    level,
-    score
-])
