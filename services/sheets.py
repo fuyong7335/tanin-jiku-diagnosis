db51d2b9c83023dbb5a03ba40a42dcb5815cefd7
fuyong7335 <yukie353yukie@gmail.com>
2025-12-17 20:20:17 +0900

    Add files via upload

--- services/sheets.py
+++ services/sheets.py
@@ -1,27 +1,19 @@
-import os
-import json
-import gspread
-from google.oauth2.service_account import Credentials
-
-def get_client():
-    service_account_info = json.loads(
-        os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"]
-    )
-
-    scopes = [
-        "https://www.googleapis.com/auth/spreadsheets",
-        "https://www.googleapis.com/auth/drive"
-    ]
-
-    creds = Credentials.from_service_account_info(
-        service_account_info,
-        scopes=scopes
-    )
-
-    return gspread.authorize(creds)
-
 def save_result(level, score):
-    client = get_client()
-    sheet = client.open_by_key("1f9nZ2SW43Q86UEH1hiAeAn10ZNf-jYWfiQbl65SB2C0").sheet1
-    sheet.append_row([level, score])
+    try:
+        client = get_client()
+        sheet = client.open_by_key("1f9nZ2SW43Q86UEH1hiAeAn10ZNf-jYWfiQbl65SB2C0").sheet1
+        sheet.append_row([level, score])
+    except Exception as e:
+        print("=== SHEETS ERROR ===")
+        print(e)
+        raise
 
+from datetime import datetime
+
+timestamp = datetime.now().isoformat()
+sheet.append_row([
+    timestamp,
+    email,
+    level,
+    score
+])
