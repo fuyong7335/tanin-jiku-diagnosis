608b2ad649297cba7f53ce1bc0fbce3c2ed8f0e2
fuyong7335 <yukie353yukie@gmail.com>
2025-12-31 23:46:04 +0900

    Add files via upload

--- services/sheets.py
+++ services/sheets.py
@@ -8,21 +8,21 @@ from google.oauth2.service_account import Credentials
 
 
 def get_client():
-    service_account_info = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"])
+    info = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"])
 
     scopes = [
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive",
     ]
 
-    creds = Credentials.from_service_account_info(service_account_info, scopes=scopes)
+    creds = Credentials.from_service_account_info(info, scopes=scopes)
     return gspread.authorize(creds)
 
 
 def save_message(level: str, score: str, message: str):
     client = get_client()
 
-    sheet_key = os.environ["SPREADSHEET_KEY"]  # Renderの環境変数に入れる
+    sheet_key = os.environ["SPREADSHEET_KEY"]  # Renderに入れる
     sheet = client.open_by_key(sheet_key).sheet1
 
     timestamp = datetime.now().isoformat(timespec="seconds")
